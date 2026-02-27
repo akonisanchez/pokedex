463429b3bb0931f793a7bb54eb216c2abbf7b26e
akonisanchez <sanchezakoni@gmail.com>
2026-02-26 19:16:52 -0800

    Add Pokemon search route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Create the Pokedex web application
 app = Flask(__name__)
@@ -8,6 +8,18 @@ app = Flask(__name__)
 @app.get("/")
 def pokedex_home():
     return render_template("index.html")
+
+# Show Pokemon route
+# This runs when browser visits /pokemon
+@app.get("/pokemon")
+def show_pokemon():
+    name = request.args.get("name")
+
+    return f"""
+    <h1>You searched for: {name}</h1>
+    <p>This is where Pokemon data will appear.</p>
+    <a href="/">Go back </a>
+    """
 
 # Start the server
 if __name__ == "__main__":
